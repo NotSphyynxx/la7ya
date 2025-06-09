796e83861e4c6f6508335f4f4edb6cda14facf98
Ismail Larhrib <ilarhrib@e3r9p13.1337.ma>
2025-06-09 20:34:35 +0100

    ready

--- mokshell/execution.c
+++ mokshell/execution.c
@@ -6,7 +6,7 @@
 /*   By: ilarhrib <ilarhrib@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/08 15:01:33 by ilarhrib          #+#    #+#             */
-/*   Updated: 2025/06/09 20:21:19 by ilarhrib         ###   ########.fr       */
+/*   Updated: 2025/06/09 20:34:23 by ilarhrib         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,18 +15,6 @@
 void	cmnd_check(char **input, char **envp, t_token *tokens)
 {
 	g_flag_signal = 1;
-	t_token *cur = tokens;
-	while (cur)
-	{
-		printf("tokens --> %s\n", cur->value);
-		cur = cur->next;
-	}
-	char **p = input;
-	while (*p)
-	{
-		printf("input --> %s\n", *p);
-		p++;
-	}
 	if (tokens && !handle_check())
 	{
 		if (contains_pipe_in_tokens(tokens))
@@ -91,7 +79,7 @@ void	execute_pipe_commands(t_token *tokens)
 			{
 				free(data->pids);
 				data->pids = NULL;
-				return;
+				return ;
 			}
 			idx++;
 		}
